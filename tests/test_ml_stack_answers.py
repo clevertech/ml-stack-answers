1f58045733fc86f1f267c2b427f405c3857d5564
Chad <chad@chadfurman.com>
2018-08-30 21:42:17 -0400

    added tests for missing file checks

--- tests/test_ml_stack_answers.py
+++ tests/test_ml_stack_answers.py
@@ -9,6 +9,16 @@ from click.testing import CliRunner
 
 from ml_stack_answers import cli
 
+files = [
+    'Badges.xml',
+    'Comments.xml',
+    'PostHistory.xml',
+    'PostLinks.xml',
+    'Posts.xml',
+    'Tags.xml',
+    'Users.xml',
+    'Votes.xml',
+]
 
 @pytest.fixture
 def response():
@@ -32,3 +42,23 @@ def test_command_line_interface():
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help' in help_result.output
+    assert 'preprocess' in help_result.output
+
+def test_default_data_dir():
+    """Test the CLI."""
+    print("This test assumes that ./data/ contains all relevant files")
+    runner = CliRunner()
+    data_result = runner.invoke(cli.main, ['preprocess'])
+    for file in files:
+        assert file + "is found." in data_result.output
+    assert "Missing files." not in data_result.output
+
+def test_invalid_data_dir():
+    """Test the CLI."""
+    print("This test assumes that ./data/ contains all relevant files")
+    runner = CliRunner()
+    data_result = runner.invoke(cli.main, ['preprocess', '--data', '/tmp'])
+    for file in files:
+        assert file + "is not found." in data_result.output
+    assert "Missing files" in data_result.output
+    assert data_result.exit_code == 100
